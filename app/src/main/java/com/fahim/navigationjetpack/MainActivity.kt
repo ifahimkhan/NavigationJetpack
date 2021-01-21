d5cd70ceb667bdb70a45a8894baf63fe54c8ba56
Mohammed Fahim khan <42889314+ifahimkhan@users.noreply.github.com>
2021-01-21 14:31:23 +0530

    init commit

---
+++ app/src/main/java/com/fahim/navigationjetpack/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fahim.navigationjetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
